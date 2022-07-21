2d6edba65d994ca0f226665060f876ccd691b68e
Akshat Mittal <akshatmittal2506@gmail.com>
2022-07-21 23:14:59 +0530

    created data for team

---
+++ src/team.js
@@ -0,0 +1,498 @@
+import kashishArora from "./people/kashishArora.jpeg";
+import anjaliSrivastava from "./people/anjaliSrivastava.jpeg";
+import aaryanGupta from "./people/aaryanGupta.jpg";
+import jitakshKapoor from "./people/jitakshKapoor.jpg";
+import pranjalJoshi from "./people/pranjalJoshi.jpg";
+import bhavyaArora from "./people/bhavyaArora.jpg";
+import apurv from "./people/apurv.jpeg";
+import swarnimaTiwari from "./people/swarnimaTiwari.jpg";
+import umangSharma from "./people/umangSharma.jpeg";
+import vanshikaPandey from "./people/vanshikaPandey.jpg";
+import siddharthSharma from "./people/siddharthSharma.jpeg";
+import muskanAgrawal from "./people/muskanAgrawal.png";
+import adityaSingh from "./people/adityaSingh.jpeg";
+import kushagraJohari from "./people/kushagraJohari.jpg";
+import priyaSharma from "./people/priyaSharma.jpeg";
+import shubhiDua from "./people/shubhiDua.jpg";
+import ankurAnand from "./people/ankurAnand.jpg";
+import mahavirDabas from "./people/mahavirDabas.png";
+import shivanandShukla from "./people/shivanandShukla.jpeg";
+import siddhantKaashikar from "./people/siddhantKaashikar.jpeg";
+import varunProhit from "./people/varunProhit.jpg";
+import rishiSrivastava from "./people/rishiSrivastava.jpg";
+import sarthakVishnu from "./people/sarthakVishnu.jpg";
+import sajalRuhela from "./people/sajalRuhela.jpg";
+import nimishTiwari from "./people/nimishTiwari.jpg";
+import akshatMittal from "./people/akshatMittal.jpg";
+import akshatKhosya from "./people/akshatKhosya.jpeg";
+import prathamSingh from "./people/prathamSingh.jpeg";
+import shubhiArora from "./people/shubhiArora.jpg";
+import anubhavGoyal from "./people/anubhavGoyal.jpeg";
+import srijanOjha from "./people/srijanOjha.jpeg";
+import pranavJalan from "./people/pranavJalan.jpg";
+import anurag from "./people/anurag.jpeg";
+import krishakChhura from "./people/krishakChhura.jpg";
+import aditiChauhan from "./people/aditiChauhan.jpeg";
+import sayakMondal from "./people/sayakMondal.png";
+import vanshSingh from "./people/vanshSingh.jpg";
+import ketanSrivastava from "./people/ketanSrivastava.jpeg";
+import chelsiJain from "./people/chelsiJain.jpg";
+import mitaliJain from "./people/mitaliJain.jpg";
+const people = [
+	{
+		email: "itsumangsharma@gmail.com",
+		name: "Umang sharma",
+		role: "Co-Coordinator",
+		subtitle: "PR",
+		ld: "https://linkedin.com/in/0729",
+		gh: "https://github.com/sharmaumang001",
+		tel: 919877918681,
+		img: umangSharma,
+	},
+	{
+		email: "19155@iiitu.ac.in",
+		name: "Vanshika Pandey",
+		role: "Co-Coordinator",
+		subtitle: "Web Developer",
+		ld: "https://www.linkedin.com/in/vanshika-pandey-a590011a9",
+		gh: "https://github.com/vanshikapandey",
+		tel: 8787050677,
+		img: vanshikaPandey,
+	},
+	{
+		email: "iamsiddharth2207@gmail.com",
+		name: "Siddharth Sharma",
+		role: "Co-Coordinator",
+		subtitle: "ML engineer",
+		ld: "https://www.linkedin.com/in/siddharth-sharma-6a2a4b18b",
+		gh: "https://github.com/SiddharthSharma2207",
+		tel: 6397005918,
+		img: siddharthSharma,
+	},
+	{
+		email: "19229@iiitu.ac.in",
+		name: "Muskan Agrawal",
+		role: "Co-Coordinator",
+		subtitle: "Competitive Programmer and Web Developer",
+		ld: "https://www.linkedin.com/in/muskan-agrawal-425211191",
+		gh: "https://github.com/muskan26062000",
+		tel: 918979738138,
+		img: muskanAgrawal,
+	},
+	{
+		email: "kashisharora4110@gmail.com",
+		name: "Kashish Arora",
+		role: "Coordinator",
+		subtitle: "MEAN stack developer | Event Management",
+		ld: "https://www.linkedin.com/in/kashish-arora11/",
+		gh: "https://github.com/kashish-arora",
+		tel: 9501132476,
+		img: kashishArora,
+	},
+	{
+		email: "19106@iiitu.ac.in",
+		name: "Aditya Singh",
+		role: "Co-Coordinator",
+		subtitle: "Mern Stack developer",
+		ld: "https://www.linkedin.com/in/thisisadityasingh/",
+		gh: "https://github.com/Aditya-code-collab",
+		tel: 8905801104,
+		img: adityaSingh,
+	},
+	{
+		email: "akshatmittal2506@gmail.com",
+		name: "Akshat Mittal",
+		role: "Executive Member",
+		subtitle: "MERN Stack Developer",
+		ld: "https://www.linkedin.com/in/akshat-mittal-851073202",
+		gh: "https://www.github.com/akshatmittal61",
+		tel: 9456849466,
+		img: akshatMittal,
+	},
+	{
+		email: "akshaatdps12@gmail.com",
+		name: "Akshat Khosya",
+		role: "Executive Member",
+		subtitle: "MERN STACK DEVELOPER",
+		ld: "https://www.linkedin.com/in/akshat-khosya-7b9321201/",
+		gh: "https://github.com/akshat-khosya/epmoc/issues",
+		tel: 9416908474,
+		img: akshatKhosya,
+	},
+	{
+		email: "20163@iiitu.ac.in",
+		name: "Varun Prohit",
+		role: "Executive Member",
+		subtitle: "Competitive Programmer | Frontend Developer",
+		ld: "https://www.linkedin.com/in/varun-prohit-a33939201",
+		gh: "https://github.com/VarunProhit",
+		tel: 8103325733,
+		img: varunProhit,
+	},
+	{
+		email: "20208@iiitu.ac.in",
+		name: "Chelsi Jain",
+		role: "Executive Member",
+		subtitle: "Web Developer",
+		ld: "https://www.linkedin.com/in/chelsi-jain-50967b205",
+		gh: "https://github.com/Chelsijain20",
+		tel: 9057565604,
+		img: chelsiJain,
+	},
+	{
+		email: "itzmitali03@gmail.com",
+		name: "Mitali Jain",
+		role: "Executive Member",
+		subtitle: "MERN Stack Developer",
+		ld: "https://www.linkedin.com/in/mitali-jain-6b4b1a204",
+		gh: "https://github.com/Mitalijain3",
+		tel: 7737149963,
+		img: mitaliJain,
+	},
+	{
+		email: "18222@iiitu.ac.in",
+		name: "Anjali Srivastava ",
+		role: "Coordinator",
+		subtitle: "",
+		ld: "http://linkedin.com/in/anjali-srivastava-939852178",
+		gh: "https://github.com/AnjaliSrivastava722",
+		tel: 9140771599,
+		img: anjaliSrivastava,
+	},
+	{
+		email: "18213@iiitu.ac.in",
+		name: "Aaryan Gupta",
+		role: "Coordinator",
+		subtitle: "Software developer",
+		ld: "https://www.linkedin.com/in/aaryan-gupta-58b360171",
+		gh: "github.com/AaryanG",
+		tel: 7006298990,
+		img: aaryanGupta,
+	},
+	{
+		email: "ykaps1707@gmail.com",
+		name: "Jitaksh Kapoor",
+		role: "Coordinator",
+		subtitle: "Penetration Tester",
+		ld: "https://www.linkedin.com/in/jitaksh-kapoor",
+		gh: "https://github.com/JitakshKapoor",
+		tel: 7696166014,
+		img: jitakshKapoor,
+	},
+	{
+		email: "19315@iiitu.ac.in",
+		name: "Kushagra Johari",
+		role: "Co-Coordinator",
+		subtitle: "Web Developer",
+		ld: "https://www.linkedin.com/",
+		gh: "https://github.com/joharikushagra",
+		tel: 917668987397,
+		img: kushagraJohari,
+	},
+	{
+		email: "priyasharma972046@gmail.com",
+		name: "priya sharma",
+		role: "Co-Coordinator",
+		subtitle: "Full Stack Developer",
+		ld: "https://www.linkedin.com/in/priya-sharma-a58211191/",
+		gh: "https://github.com/PriyaSharma3",
+		tel: 6303617871,
+		img: priyaSharma,
+	},
+	{
+		email: "pranjal.ksp@gmail.com",
+		name: "Pranjal Joshi",
+		role: "Coordinator",
+		subtitle: "Backend Developer",
+		ld: "https://www.linkedin.com/in/pranjaljoshi05/",
+		gh: "https://github.com/PranjalJoshi05",
+		tel: 8006980617,
+		img: pranjalJoshi,
+	},
+	{
+		email: "18221@iiitu.ac.in",
+		name: "Bhavya Arora",
+		role: "Coordinator",
+		subtitle: "",
+		ld: "https://www.linkedin.com/in/bhavya-arora-5b6978175/",
+		gh: "https://github.com/Bhavya00",
+		tel: 9680180736,
+		img: bhavyaArora,
+	},
+	{
+		email: "19240@iiitu.ac.in",
+		name: "Shubhi Dua ",
+		role: "Co-Coordinator",
+		subtitle: "Java | Data structures | Algorithms ",
+		ld: "https://www.linkedin.com/in/shubhi-dua-65a1a9191",
+		gh: "",
+		tel: 8979666098,
+		img: shubhiDua,
+	},
+	{
+		email: "18211@iiitu.ac.in",
+		name: "Apurv",
+		role: "Coordinator",
+		subtitle: "NLP Engineer",
+		ld: "https://www.linkedin.com/in/apurvsj",
+		gh: "https://github.com/apurv0303",
+		tel: 6204252595,
+		img: apurv,
+	},
+	{
+		email: "18230@iiitu.ac.in",
+		name: "Swarnima Tiwari ",
+		role: "Coordinator",
+		subtitle: "",
+		ld: "https://www.linkedin.com/in/swarnima-tiwari-8b9306179",
+		gh: "https://github.com/swarnima14",
+		tel: 8384859321,
+		img: swarnimaTiwari,
+	},
+	{
+		email: "19208@iiitu.ac.in",
+		name: "Ankur Anand",
+		role: "Co-Coordinator",
+		subtitle: "Data Scientist / Junior Developer",
+		ld: "https://www.linkedin.com/in/anandankur2816/",
+		gh: "https://github.com/anandankur2816",
+		tel: 918102036794,
+		img: ankurAnand,
+	},
+	{
+		email: "mahavirdabas18@gmail.com",
+		name: "Mahavir Dabas",
+		role: "Co-Coordinator",
+		subtitle: "",
+		ld: "https://www.linkedin.com/in/mahavir-dabas-8b920a191/",
+		gh: "https://github.com/MahavirDabas18",
+		tel: 9958703889,
+		img: mahavirDabas,
+	},
+	{
+		email: "shivanandisshukla@gmail.com",
+		name: "Shivanand Shukla",
+		role: "Co-Coordinator",
+		subtitle: "Data Analyst",
+		ld: "https://www.linkedin.com/in/shivanand-shukla-855382191/",
+		gh: "https://github.com/XS-Shiv",
+		tel: 9521007325,
+		img: shivanandShukla,
+	},
+	{
+		email: "19241@iiitu.ac.in",
+		name: "Siddhant Kaashikar",
+		role: "Co-Coordinator",
+		subtitle: "Digital Design Engineer",
+		ld: "https://www.linkedin.com/in/skaashikar/",
+		gh: "",
+		tel: 917350014149,
+		img: siddhantKaashikar,
+	},
+	{
+		email: "krishakchhura@gmail.com",
+		name: "Krishak Chhura",
+		role: "Executive Member",
+		subtitle: "Member of surge club",
+		ld: "https://www.linkedin.com/in/krishak-chhura-52b924201",
+		gh: "",
+		tel: 700992379,
+		img: krishakChhura,
+	},
+	{
+		email: "aditi.iiitu@gmail.com",
+		name: "Aditi Chauhan",
+		role: "Executive Member",
+		subtitle: "Tech Enthusiast",
+		ld: "https://www.linkedin.com/in/aditichauhan-",
+		gh: "https://github.com/aditichauhan04",
+		tel: 8126046622,
+		img: aditiChauhan,
+	},
+	{
+		email: "20155@iiitu.ac.in",
+		name: "Sayak Mondal",
+		role: "Executive Member",
+		subtitle: "Video Editor, Graphic designer",
+		ld: "https://www.linkedin.com/in/sayak-mondal-b70384200/",
+		gh: "https://github.com/sayak22",
+		tel: 8373072231,
+		img: sayakMondal,
+	},
+	{
+		email: "20230@iiitu.ac.in",
+		name: "Pranav Jalan",
+		role: "Executive Member",
+		subtitle: "Graphic Designer",
+		ld: "https://www.linkedin.com/in/pranav-jalan-548ba3228/",
+		gh: "",
+		tel: 9024506027,
+		img: pranavJalan,
+	},
+	{
+		email: "vanshsingh1617@gmail.com",
+		name: "Vansh Singh",
+		role: "Executive Member",
+		subtitle: "Full Stack Developer | UI/UX Designer",
+		ld: "https://www.linkedin.com/in/vansh-singh-0414",
+		gh: "https://github.com/Va1nsh14",
+		tel: 9569649636,
+		img: vanshSingh,
+	},
+	{
+		email: "prathambsingh891@gmail.com",
+		name: "Pratham Singh",
+		role: "Executive Member",
+		subtitle: "Web Developer",
+		ld: "https://www.linkedin.com/in/pratham-singh-596591203/",
+		gh: "https://github.com/pratham891",
+		tel: 7600662459,
+		img: prathamSingh,
+	},
+	{
+		email: "20158@iiitu.ac.in",
+		name: "Shubhi Arora",
+		role: "Executive Member",
+		subtitle: "Competitive Programmer | Full Stack Web Developer",
+		ld: "https://www.linkedin.com/in/shubhi-arora-15a549205/",
+		gh: "https://github.com/shubhi-arora",
+		tel: 7668191238,
+		img: shubhiArora,
+	},
+	{
+		email: "20116@iiitu.ac.in",
+		name: "Anubhav Goyal",
+		role: "Executive Member",
+		subtitle: "Front End Developer",
+		ld: "https://www.linkedin.com/in/anubhav-goyal-962744211",
+		gh: "https://github.com/G-Anubhav",
+		tel: 7689903145,
+		img: anubhavGoyal,
+	},
+	{
+		email: "20247@iiitu.ac.in",
+		name: "Srijan Ojha",
+		role: "Executive Member",
+		subtitle: "Fronted Developer",
+		ld: "https://www.linkedin.com/in/srijan-ojha-074a02212",
+		gh: "",
+		tel: 9336744146,
+		img: srijanOjha,
+	},
+	{
+		email: "20226@iiitu.ac.in",
+		name: "Nimish Tiwari",
+		role: "Executive Member",
+		subtitle: "Data Science enthusiast",
+		ld: "https://www.linkedin.com/in/nimish-tiwari-iiitu-5b4091229",
+		gh: "https://github.com/phoenix-maker",
+		tel: 9665499462,
+		img: nimishTiwari,
+	},
+	{
+		email: "20204@iiitu.ac.in",
+		name: "Anurag",
+		role: "Executive Member",
+		subtitle: "Web development and poster making",
+		ld: "https://www.linkedin.com/in/anurag-govind-552977228",
+		gh: "https://github.com/GovindAnurag",
+		tel: 7007210720,
+		img: anurag,
+	},
+	{
+		email: "20216@iiitu.ac.in",
+		name: "Ketan srivastava",
+		role: "Executive Member",
+		subtitle: "",
+		ld: "https://www.linkedin.com/in/ketan-srivastava-459519215/",
+		gh: "https://github.com/Ketansri22",
+		tel: 9958633580,
+		img: ketanSrivastava,
+	},
+	{
+		email: "rishisr.9044@gmail.com",
+		name: "Rishi Srivastava",
+		role: "Executive Member",
+		subtitle: "Competitive Programmer, Frontend Developer",
+		ld: "https://www.linkedin.com/in/rishi-sr",
+		gh: "https://github.com/RishiSr",
+		tel: 7985714918,
+		img: rishiSrivastava,
+	},
+	{
+		email: "20240@iiitu.ac.in",
+		name: "Sarthak Vishnu",
+		role: "Executive Member",
+		subtitle: "Data Science enthusiast",
+		ld: "https://www.linkedin.com/in/sarthak-vishnu-iiitu-563729201/",
+		gh: "https://github.com/Sarthak-Vishnu",
+		tel: 8080572559,
+		img: sarthakVishnu,
+	},
+	{
+		email: "ruhela.sajal017@gmail.com",
+		name: "Sajal Ruhela",
+		role: "Executive Member",
+		subtitle: "Competitive Programmer",
+		ld: "www.linkedin.com/in/sajal-ruhela-688534219",
+		gh: "https://github.com/SajalRuhela017",
+		tel: 8077948591,
+		img: sajalRuhela,
+	},
+];
+console.log(people.length);
+let cooridnators = [];
+let cocooridnators = [];
+let executives = [];
+let volunterrs = [];
+
+people.forEach((person) => {
+	let newObj = {
+		name: person.name,
+		subtitle: person.subtitle,
+		image: person.img,
+		socials: [
+			{
+				handle: "linkedin",
+				link: person.ld,
+				username: person.ld.slice(28),
+			},
+			{
+				handle: "github",
+				link: person.gh,
+				username: person.gh.slice(19),
+			},
+			{
+				handle: "phone",
+				link: `tel:${person.tel}`,
+				username: `${person.tel}`,
+			},
+			{
+				handle: "email",
+				link: `mailto:${person.email}`,
+				username: `${person.email}`,
+			},
+		],
+	};
+	if (person.role === "Coordinator") cooridnators = [...cooridnators, newObj];
+	if (person.role === "Co-Coordinator")
+		cocooridnators = [...cocooridnators, newObj];
+	if (person.role === "Executive Member")
+		executives = [...executives, newObj];
+	if (person.role === "Volunteer") volunterrs = [...volunterrs, newObj];
+});
+let teams = [
+	{
+		teamTitle: "Coordinators",
+		members: cooridnators,
+	},
+	{
+		teamTitle: "Co-Coordinators",
+		members: cocooridnators,
+	},
+	{
+		teamTitle: "Executive Members",
+		members: executives,
+	},
+];
+export default teams;
