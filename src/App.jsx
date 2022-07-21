42ff807395d156805f7a76b820c165fed0a052d8
Akshat Mittal <akshatmittal2506@gmail.com>
2022-07-21 23:11:17 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./style.css";
+
+const App = () => {
+	AOS.init();
+	return (
+		<>
+		</>
+	);
+};
+
+export default App;
