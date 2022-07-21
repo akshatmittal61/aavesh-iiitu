9b8e70b8244fce5d6e9ae4cda28094ea339dabe0
Akshat Mittal <akshatmittal2506@gmail.com>
2022-07-21 23:13:13 +0530

    created context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./style.css";
+import Home from "./pages/Home/Home";
 
 const App = () => {
 	AOS.init();
 	return (
 		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
 		</>
 	);
 };
