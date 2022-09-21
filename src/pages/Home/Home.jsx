50f6e2f2ad4af20c0fd14297ad64148d49437175
Akshat Mittal <akshatmittal2506@gmail.com>
2022-09-21 20:31:48 +0530

    changed home and footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 					</div>
 					<div className="home-hero-right-bottom">
 						<span data-aos="fade-left">
-							Bringing the future to masses.
+							Creating ideas with technology and imagination
 						</span>
 					</div>
 				</div>
@@ -34,6 +34,30 @@ const Home = () => {
 				>
 					<MaterialIcons>expand_more</MaterialIcons>
 				</button>
+			</section>
+			<section className="home-about">
+				<div className="home-about-head">
+					<h1>About Us</h1>
+				</div>
+				<div className="home-about-body">
+					<p>
+						Aavesh is an electronics society that strives to teach
+						and help students acquire new skills in an era of
+						rapidly evolving technology in the field of electronics
+						and communication engineering. The Society aims to
+						provide potential minds of IIITU a platform to discuss
+						innovative ideas and nurture their skills to tackle real
+						life challenges in the world today and contribute to the
+						technical society through various projects, patents and
+						research publications. We will be organizing various
+						expert lectures, workshops, mentorship programs as well
+						as competitions throughout the year to keep the students
+						learning and providing them the necessary resources to
+						implement their ideas. Activities of this club are
+						published in the Current event section in every
+						semester.
+					</p>
+				</div>
 			</section>
 		</main>
 	);
